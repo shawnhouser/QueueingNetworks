8fa36b719cafc0de7b2f9dec4aff4211a43143ff
Angyalka Valcsics <36708283+angyalkavalcsics@users.noreply.github.com>
2021-11-10 17:37:06 -0500

    Update openJacksonNetworks.R

--- openJacksonNetworks.R
+++ openJacksonNetworks.R
@@ -2,8 +2,8 @@
 # Open Jackson Networks
 ################################################################################
 # inputs: routing matrix R, input rates from outside to each node gamma, 
-# service rates of each node mu
-ojn.summary <- function(R, gamma, mu){
+# service rates of each node mu, maxMarginalProb is how many marginals are returned
+ojn.summary <- function(R, gamma, mu, maxMarginalProb){
    n = nrow(R)
    m = ncol(R)
    K = length(mu)
@@ -18,25 +18,22 @@ ojn.summary <- function(R, gamma, mu){
    Wq_i = Lq_i / lambda_i
    L = sum(L_i)
    W = L/(sum(gamma))
-   # are the marginals just rho_i ^ n?
+   # find marginals
+   p_n = matrix(0, maxMarginalProb, K)
+   p_n[1,] = 1-rho_i
+   for (i in 1:(maxMarginalProb-1)) {
+      p_n[i+1,]=(rho_i^i)*(1-rho_i)
+   }
    rnames <- rbind("Node i throughput", "Utilization at node i", 
                    "Probability node i is idle", "Mean time at node i","Mean number at node i",
                    "Mean queue time at node i", "Mean number in queue at node i")
    res <- data.frame(cbind(rnames, round(rbind(lambda_i, rho_i, I_i, W_i, L_i, Wq_i, Lq_i), 4)))
    names(res)<- c("Definition", sprintf("node%2d",seq(1:K)))
-   # 'marginal_probabilities' = p, 
-   return(list('res' = res,
+   return(list('marginal_probabilities' = p_n, 'res' = res,
                'throughput_i' = lambda_i, 'rho_i' = rho_i, 'idle' = I_i, 
                'W_i' = W_i, 'L_i' = L_i, 'Wq_i' = Wq_i, 'Lq_i' = Lq_i,
                'W' = W, 'L' = L))
 }
-
-
-
-
-
-
-
 
 
 
